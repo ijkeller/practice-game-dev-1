85293983afcb33aa82783e092742485ecb93d70e
Isaiah Keller <isaiah.keller@gmail.com>
2022-07-28 13:15:59 -0700

    added some assets and started basic animation

--- one/script.js
+++ one/script.js
@@ -1,3 +1,15 @@
 const canvas = document.getElementById('canvas-1');
 const ctx = canvas.getContext('2d');
+const CANVAS_WIDTH = canvas.width = 600;
+const CANVAS_HEIGHT = canvas.height = 600;
 
+const playerImage = new Image();
+playerImage.src = './assets/shadow_dog.png'
+
+function animate(){
+    ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    ctx.fillRect(50,50,100,100);
+    ctx.drawImage(playerImage, 0, 0);
+    requestAnimationFrame(animate);
+};
+animate();
